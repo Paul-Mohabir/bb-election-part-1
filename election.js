022f7f87be5a084484fc8fef7ba5a59350571377
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-02-20 14:52:08 -0500

    added ul to index and json data to .js

--- election.js
+++ election.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
+ $.ajax
+ url: url: 'https://bb-election-api.herokuapp.com/',
+ method: 'Get',
+ dataType: 'Json'
+}).done(function(data){
 
-  // Imagination!
-
+    for(var y=0; y < data.candidates.length; y++) {
+    $('#list').append('<li> Name: ' + data.candidates[y].name + '</li>')
+    $('#list').append('<li> Votes: ' + data.candidates[y].votes + '</li><br>')
+    };
+  });
 });
